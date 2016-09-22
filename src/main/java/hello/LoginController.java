3e197892d1c9c82220b964436c6ca65c301e3385
Quang Le <quang.le@stash.itsoninc.com>
2016-09-22 14:17:23 +0700

    Add project

---
+++ src/main/java/hello/LoginController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author quangle
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+//    @RequestMapping(path = "/do-login", method = RequestMethod.POST)
+//    public String doLogin(
+//            @RequestParam(value = "uname", required = true) String userName,
+//            @RequestParam(value = "psw", required = true) String password,
+//            Model model) {
+//        if(userName.equals("mot") && password.equals("123")) {
+//            return "redirect:/greeting";
+//        } else {
+//            return "login-fail";
+//        }
+//    }
+    
+    @RequestMapping(path = "/do-login")
+    public String doLogin(
+            @RequestParam(value = "uname", required = true) String userName,
+            @RequestParam(value = "psw", required = true) String password,
+            Model model) {
+        if(userName.equals("mot") && password.equals("123")) {
+            return "redirect:/greeting";
+        } else {
+            return "login-fail";
+        }
+    }
+}
